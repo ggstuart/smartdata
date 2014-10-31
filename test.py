d3a5634659ccd6292e0bb6858dc563d624f90384
Graeme Stuart <ggstuart@gmail.com>
2014-10-31 12:39:50 +0000

    log, don't print

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import logging
 from smartdata import SmartspacesCSVAdapter, SmartspacesJSONAdapter
 
-#log = logging.getLogger('test')
+log = logging.getLogger('test')
 logging.basicConfig(level=logging.INFO)
 
 csv = SmartspacesCSVAdapter()
@@ -9,8 +9,8 @@ json = SmartspacesJSONAdapter()
 
 for org in json.organisations():
     for b in json.buildings(org):
-        print "%s: %s" % (org['name'], b['name'])
+        log.info("%s: %s" % (org['name'], b['name']))
         for m in json.meters(b):
-            print "\t%s (%s)" % (m['commodity'], m['name'])
+            log.info("%s (%s)" % (m['commodity'], m['name']))
             data = csv.dataframe(m['id'], 'all')
 
